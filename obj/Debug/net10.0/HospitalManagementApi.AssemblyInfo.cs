38ea254ca373a001e9a7357ba1c5101a021b46e2
Gourav Khore <gouravkhore656@gmail.com>
2026-09-23 18:42:59 +0530

    updating

--- obj/Debug/net10.0/HospitalManagementApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/HospitalManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15c580c9b2ff7e09268ff7f2dfbe5356b353fb57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e11d8b46b8ca05754e0d41e94cdf96ff3b7a5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
